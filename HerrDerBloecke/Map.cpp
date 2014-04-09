54a1d41b8b2ff76d9b17548a8b0af7abb0dafae8
Florian Meißner <developer@mystler.eu>
2014-04-09 13:14:42 +0200

    Map revamped

--- HerrDerBloecke/Map.cpp
+++ HerrDerBloecke/Map.cpp
@@ -4,18 +4,15 @@
 using namespace System::IO;
 using namespace System::Diagnostics;
 
-#define FIELDS_X 1280 // Only for one side ==> total is double + 1 (center)
-#define FIELDS_Y 1280 // Only for one side ==> total is double + 1 (center)
+#define FIELDS_X 255
+#define FIELDS_Y 255
 #define FIELD_WIDTH 1.f
 #define FIELD_HEIGHT 1.f
 
-/************
- * MapField *
- ************/
-
-HdB::MapField::MapField(Device^ device, String^ texture)
-    : mDevice(device)
+HdB::Map::Map(Device^ device)
+: mDevice(device)
 {
+    String^ texture = "Grass.png";
     try {
         mTexture = Texture::FromFile(mDevice, TEXTURE_PATH + texture, Usage::None, Pool::Managed);
     } catch (Exception^ e) {
@@ -23,7 +20,7 @@ HdB::MapField::MapField(Device^ device, String^ texture)
         Debug::WriteLine("ERROR: Could not load texture " + texture);
     }
 
-    Mesh^ temp = Mesh::CreateBox(mDevice, FIELD_WIDTH, FIELD_HEIGHT, 0.f);
+    Mesh^ temp = Mesh::CreateBox(mDevice, FIELDS_X * FIELD_WIDTH, FIELDS_Y * FIELD_HEIGHT, 0.f);
     mGroundMesh = temp->Clone(mDevice, temp->CreationOptions, VertexFormat::Position | VertexFormat::Texture1);
     delete temp;
     DataStream^ dataStream = mGroundMesh->LockVertexBuffer(LockFlags::None);
@@ -32,12 +29,10 @@ HdB::MapField::MapField(Device^ device, String^ texture)
     dataStream->Seek(0, SeekOrigin::Begin);
     for each (Vector3% v in vectors) {
         dataStream->Position += Vector3::SizeInBytes;
-        dataStream->Write(v.X == box.Maximum.Y ? 1.f : 0.f);
-        dataStream->Write(v.Y == box.Maximum.Y ? 0.f : 1.f);
+        dataStream->Write(v.X == box.Maximum.X ? FIELDS_X : 0.f);
+        dataStream->Write(v.Y == box.Maximum.Y ? 0.f : FIELDS_Y);
     }
     dataStream->Close();
-
-    mInstances = gcnew List<Vector3>();
 
     // Default material
     mMaterial.Diffuse = Color4(.6f, .6f, .6f);
@@ -45,51 +40,17 @@ HdB::MapField::MapField(Device^ device, String^ texture)
     mMaterial.Specular = Color4(.9f, .9f, .9f);
 }
 
-HdB::MapField::~MapField()
+HdB::Map::~Map()
 {
-    mInstances->Clear();
     delete mGroundMesh;
     delete mTexture;
 }
 
-void HdB::MapField::Draw()
+void HdB::Map::Draw()
 {
     mDevice->Material = mMaterial;
     mDevice->SetTexture(0, mTexture);
-
-    for each (Vector3% field in mInstances) {
-        mDevice->SetTransform(TransformState::World, Matrix::Translation(field));
-        mGroundMesh->DrawSubset(0);
-    }
-}
-
-void HdB::MapField::AddField(int x, int y)
-{
-    Vector3 pos(x * FIELD_WIDTH, y * FIELD_HEIGHT, 0.f);
-    mInstances->Add(pos);
-}
-
-/*******
-* Map *
-*******/
-
-HdB::Map::Map(Device^ device)
-: mDevice(device)
-{
-    mGrass = gcnew MapField(mDevice, "Grass.png");
-    for (int i = -FIELDS_X; i <= FIELDS_X; i++) {
-        for (int j = -FIELDS_Y; j <= FIELDS_Y; j++) {
-            mGrass->AddField(i, j);
-        }
-    }
-}
-
-HdB::Map::~Map()
-{
-    delete mGrass;
-}
-
-void HdB::Map::Draw()
-{
-    mGrass->Draw();
+        
+    mDevice->SetTransform(TransformState::World, Matrix::Translation(Vector3::Zero));
+    mGroundMesh->DrawSubset(0);
 }
--- HerrDerBloecke/Map.h
+++ HerrDerBloecke/Map.h
@@ -8,35 +8,10 @@ using namespace SlimDX;
 using namespace SlimDX::Direct3D9;
 
 namespace HdB {
-    ref class MapField : IDrawable
-    {
-    public:
-        /** Simple MapField that consists of only a ground plane with
-         *  a texture.
-         */
-        MapField(Device^ device, String^ texture);
-
-        virtual ~MapField();
-
-        virtual void Draw();
-
-        /** Add a new map field at tha abstract position x, y.
-         *  X and Y are not world coordinates but grid coordinates.
-         */
-        virtual void AddField(int x, int y);
-
-    protected:
-        Device^ mDevice;
-        Mesh^ mGroundMesh;
-        Texture^ mTexture;
-        List<Vector3>^ mInstances;
-        Material mMaterial;
-    };
-
     ref class Map : IDrawable
     {
     public:
-        /** Construct our renderable map. */
+        /** Construct our renderable map. Just a simple grass ground for now. */
         Map(Device^ device);
 
         virtual ~Map();
@@ -46,6 +21,8 @@ namespace HdB {
 
     private:
         Device^ mDevice;
-        MapField^ mGrass;
+        Mesh^ mGroundMesh;
+        Texture^ mTexture;
+        Material mMaterial;
     };
 }
